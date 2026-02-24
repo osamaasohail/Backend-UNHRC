eb5786e95ae5df7cf83491d266e1ceb975a0f1e2
Akif-Imtiaz <bhiakif@gmail.com>
2026-02-24 23:31:53 +0500

    Refactor chat routes to improve code clarity with inline comments for better understanding

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -6,11 +6,11 @@ import { createOrGetConversation,getUserConversations ,getConversationById} from
 const router = Router();
 
 
-router.route("/chat-users").get(verifyClerkToken,getChatUsers);
-router.route("/messages/:conversationId").get(verifyClerkToken, getMessagesBetweenUsers);
-router.route("/conversation/:receiverId").post(verifyClerkToken, createOrGetConversation);
-router.route("/conversations").post(verifyClerkToken,getUserConversations);
-router.route("/conversation/:conversationId").get(verifyClerkToken,getConversationById);
+router.route("/chat-users").get(verifyClerkToken,getChatUsers); //get all users except logged in user for chat list
+router.route("/messages/:conversationId").get(verifyClerkToken, getMessagesBetweenUsers); //get all messaes by conversation ID
+router.route("/conversation/:receiverId").post(verifyClerkToken, createOrGetConversation); // create conversation in database with id for room
+router.route("/conversations").post(verifyClerkToken,getUserConversations);  //get all conversations of logged in user with last message and user details
+router.route("/conversation/:conversationId").get(verifyClerkToken,getConversationById); //get conversation users email by ID
 
 
 export default router;
